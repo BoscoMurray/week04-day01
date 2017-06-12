b1657bec1049d7451b2f89a17770e950cfd2b2a7
BoscoMurray <rossamurray@gmail.com>
2017-06-12 21:29:11 +0100

    layout and style sheet added

--- rockpaperscissors/controller.rb
+++ rockpaperscissors/controller.rb
@@ -4,10 +4,9 @@ require('pry-byebug')
 require('json')
 require_relative('models/rps_game')
 
-# get '/rps_game/:player1/:player2' do
-#   game = Rps_game.new(params["player1"], params["player2"])
-#   return game.play
-# end
+get '/' do
+  erb(:home)
+end
 
 get '/rps_game/:player1/:player2' do
   game = Rps_game.new(params["player1"], params["player2"])
